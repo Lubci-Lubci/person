b8cf671b3e7502d7674bed5dc47f4b61149a8b35
Lubci-Lubci <lubo.han@gmail.com>
2022-11-17 16:01:31 +0100

    model person

--- src/main/java/com/api/person/model/Person.java
+++ src/main/java/com/api/person/model/Person.java
@@ -1,4 +1,24 @@
 package com.api.person.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persons")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "nationality")
+    private String nationality;
+
 }
