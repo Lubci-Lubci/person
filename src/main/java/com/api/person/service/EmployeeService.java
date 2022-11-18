6c1dec332b98c20b79faa7d48480eb85065e2153
Lubci-Lubci <lubo.han@gmail.com>
2022-11-18 15:32:33 +0100

    employee service & employe service Impl

--- src/main/java/com/api/person/service/EmployeeService.java
+++ src/main/java/com/api/person/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.api.person.service;
 
+import com.api.person.model.Employee;
+import com.api.person.model.Person;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+    Person getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    boolean existsEmployeeById(long id);
+    void deleteByPersonId(long personId);
 }
--- src/main/java/com/api/person/service/EmployeeServiceImpl.java
+++ src/main/java/com/api/person/service/EmployeeServiceImpl.java
@@ -1,4 +1,34 @@
 package com.api.person.service;
 
-public class EmployeeServiceImpl {
+import com.api.person.model.Employee;
+import com.api.person.model.Person;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Person getEmployeeById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+
+    }
+
+    @Override
+    public boolean existsEmployeeById(long id) {
+        return false;
+    }
+
+    @Override
+    public void deleteByPersonId(long personId) {
+
+    }
 }
