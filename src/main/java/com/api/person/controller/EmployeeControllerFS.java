6f3f2c306e7e2a5644418d72ba7c63cf62400d97
mkralik31 <mkralik31@gmail.com>
2022-11-18 13:50:04 +0100

    EmployeeController(s) classes

---
+++ src/main/java/com/api/person/controller/EmployeeControllerFS.java
@@ -0,0 +1,4 @@
+package com.api.person.controller;
+
+public class EmployeeControllerFS {
+}
---
+++ src/main/java/com/api/person/controller/EmployeeControllerRest.java
@@ -0,0 +1,7 @@
+package com.api.person.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+public class EmployeeControllerRest {
+}
