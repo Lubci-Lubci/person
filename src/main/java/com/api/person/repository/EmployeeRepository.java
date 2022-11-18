4397283658e45e99cb8866235a7f2df255ed6a34
Lubci-Lubci <lubo.han@gmail.com>
2022-11-18 15:18:44 +0100

    employee repository repair

--- src/main/java/com/api/person/repository/EmployeeRepository.java
+++ src/main/java/com/api/person/repository/EmployeeRepository.java
@@ -1,11 +1,20 @@
 package com.api.person.repository;
 
+import com.api.person.model.Employee;
 import com.api.person.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
-public interface EmployeeRepository extends JpaRepository<Person, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Transactional
+    void deleteById(long id);
+
+    @Transactional
+    void deleteByEmployeeId(long employeeId);
 
 
 }
